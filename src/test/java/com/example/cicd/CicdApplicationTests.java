af66bd2597962c2602671308cdd05272cc9c18bc
Abdou Raouf Mohamed <abdouraoufm97@gmail.com>
2024-12-28 15:03:06 +0000

    test

--- src/test/java/com/example/cicd/CicdApplicationTests.java
+++ src/test/java/com/example/cicd/CicdApplicationTests.java
@@ -8,6 +8,11 @@ class CicdApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// si le contexte de l'application se charge correctement.
+		// Si vous souhaitez ajouter des assertions ou des tests spécifiques,
+		// implémentez-les ici. Si ce n'est pas nécessaire, supprimez ce commentaire.
+
+		throw new UnsupportedOperationException("Cette méthode doit être implémentée ou laissée intentionnellement vide.");
 	}
 
 }
