859011fb1ca0a067251d69577e9efc87d70426f4
Abdou Raouf Mohamed <abdouraoufm97@gmail.com>
2025-01-03 13:31:48 +0000

    add actuor

--- src/main/java/com/example/cicd/CicdApplication.java
+++ src/main/java/com/example/cicd/CicdApplication.java
@@ -1,5 +1,8 @@
 package com.example.cicd;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableAdminServer
 public class CicdApplication {
 
+	Logger logger = LoggerFactory.getLogger(CicdApplication.class);
 	@GetMapping("/welcome")
 	public String welcome(){
+
+		logger.info("starting my greeting methode....");
 		return "Welcome to Abdou";
 	}
 
--- src/test/java/com/example/cicd/CicdApplicationTests.java
+++ src/test/java/com/example/cicd/CicdApplicationTests.java
@@ -8,11 +8,10 @@ class CicdApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Ce test vérifie uniquement que le contexte de l'application Spring se charge correctement.
 		// si le contexte de l'application se charge correctement.
 		// Si vous souhaitez ajouter des assertions ou des tests spécifiques,
 		// implémentez-les ici. Si ce n'est pas nécessaire, supprimez ce commentaire.
-
-		throw new UnsupportedOperationException("Cette méthode doit être implémentée ou laissée intentionnellement vide.");
 	}
 
 }
